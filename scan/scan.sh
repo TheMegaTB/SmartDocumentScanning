8c84a9ec90e1f2147ffb374ae8874ba8d5a20d11
Til Blechschmidt <til@blechschmidt.de>
2016-09-23 22:26:03 +0200

    Merged sorting into scanning script

--- scan/scan.sh
+++ scan/scan.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-OUT=$1
+OUT=${1%/}
 ORIGIN=$(pwd)
 TMP_DIR=$(mktemp -d)
 
@@ -8,34 +8,54 @@ if [ -z $OUT ]; then
     exit 1
 fi
 
+function get_category() {
+    echo "Searching for QR code in $1"
+    ZBAR_OUT=$(zbarimg --raw $1 2> /dev/null)
+    if [ $? -eq 0 ]; then
+        CATEGORY=$(sed -n "/\b${ZBAR_OUT}\b/p" ${ORIGIN}/../categories | awk '{print $2}')
+        if [ ! -z "${CATEGORY}" ]; then
+            QR=${CATEGORY}
+        else
+            QR=${ZBAR_OUT}
+        fi
+        echo "Sorting into $QR"
+    fi
+}
+
 function process() {
     echo "Processing pages . . ."
     PREV=""
     COUNTER=1
     for f in $(ls -v *\.tif); do
+        # Convert to JPEG and downscale
         convert "$f" -background black -fuzz 75% -deskew 50% -trim +repage \
             -limit memory 32 -limit map 64 -units PixelsPerInch -density 72 -quality 60 -format jpg "$f.jpg"
+        # Try categorizing the image by a QR code
+        get_category "${f}.jpg"
         if [ -z $PREV ]; then
             PREV="$f.jpg"
             continue
         else
-            PAGES=""
-            $ORIGIN/is_empty.py $PREV
+            # Set the fallback category
+            if [ -z "${QR}" ]; then
+                QR="Other"
+            fi
+
+            timestamp=$(date +%s)
+
+            # Check the files for content and move it to its corresponding output folder
+            $ORIGIN/is_empty.py ${PREV}
             if [ $? -eq 0 ]; then
-                PAGES="$PREV"
+                mkdir -p "${OUT}/${QR}"
+                mv "${PREV}" "${OUT}/${QR}/${timestamp}-P1"
             fi
-            $ORIGIN/is_empty.py $f
+            $ORIGIN/is_empty.py ${f}
             if [ $? -eq 0 ]; then
-                PAGES="$PAGES $f.jpg"
+                mkdir -p "${OUT}/${QR}"
+                mv "${f}.jpg" "${OUT}/${QR}/${timestamp}-P2"
             fi
-            echo $PAGES
-            if [ ! -z "$PAGES" ]; then
-                echo "$PAGES"
-                #convert $PAGES -background black -fuzz 75% -deskew 50% -trim +repage \
-                #    +append -rotate 0 -limit memory 32 -limit map 64 -units PixelsPerInch -density 72 -quality 60 -format jpg "sheet_$COUNTER.jpg"
-            else
-                echo "Skipping empty double page!"
-            fi
+
+            QR=""
             PREV=""
         fi
 
@@ -52,6 +72,6 @@ scanimage --format=tiff -b --mode Color --resolution=600 --variance=255 --emphas
 
 process
 
-mv *.jpg $OUT/
+# mv *.jpg $OUT/
 
-#rm -r $TMP_DIR
+rm -r $TMP_DIR
