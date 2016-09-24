e388b375078f33822200152471dc295942319e17
Til Blechschmidt <til@blechschmidt.de>
2016-09-24 16:25:47 +0000

    Added processing to watch

--- scan/process_watch.sh
+++ scan/process_watch.sh
@@ -1,7 +1,19 @@
 src=${1}
 out=${2}
+origin=$(pwd)
+
 inotifywait -m "${src}" -e create -e moved_to |
     while read path action file; do
-        echo "The file '$file' appeared in directory '$path' via '$action'"
-        # do something with the file
+        echo "The file '$file' appeared in directory '$src' via '$action'"
+        if [ "$file" == "scan_done" ]; then
+            tmp=$(mktemp -d)
+
+            echo "PROCESSING @ ${tmp}"
+            mv ${src}/*.tif "${tmp}/"
+            cd "${tmp}"
+            ./scan.sh process ${tmp} ${out}
+
+            cd "${pwd}"
+            rm "${src}/scan_done"
+        fi
     done
