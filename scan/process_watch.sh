fac9703cf9874b7efc11a96436456b81d0e14d3e
Til Blechschmidt <til@blechschmidt.de>
2016-09-24 15:58:45 +0000

    Removed debug print

--- scan/process_watch.sh
+++ scan/process_watch.sh
@@ -4,7 +4,6 @@ origin=$(pwd)
 
 inotifywait -m "${src}" -e create -e moved_to |
     while read path action file; do
-        echo "The file '$file' appeared in directory '$src' via '$action'"
         if [ "$file" == "scan_done" ]; then
             tmp=$(mktemp -d)
 
