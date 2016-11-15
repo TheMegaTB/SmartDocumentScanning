253158d25ff9f3290b902ec7b5183c53cbf97ab7
TheMegaTB <til@blechschmidt.de>
2016-11-15 15:50:02 +0100

    Added image rotation

--- scan/scan.sh
+++ scan/scan.sh
@@ -32,6 +32,12 @@ function process {
         # Convert to JPEG and downscale
         convert "$f" -background black -fuzz 75% -deskew 50% -trim +repage \
             -limit memory 32 -limit map 64 -units PixelsPerInch -density 72 -quality 60 -format jpg "$f.jpg"
+	
+	# Get the rotation of the image
+	rotation=$(tesseract "${f}.jpg" stdout -psm 0 | grep "Rotate: " | awk '{print $2}')
+	echo "Rotating image by ${rotation}"
+	mogrify -rotate "${rotation}"
+
         # Try categorizing the image by a QR code
         get_category "${f}.jpg"
         if [ -z $PREV ]; then
@@ -44,7 +50,7 @@ function process {
             fi
 
             timestamp=$(date +%s)
-
+	    
             # Check the files for content and move it to its corresponding output folder
             $ORIGIN/is_empty.py ${PREV}
             if [ $? -eq 0 ]; then
